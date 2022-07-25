1082133edae78c511cbf94e0be09c50a992a0b6f
ksyz <ksyz@duck.com>
2022-07-25 13:46:34 +0200

    Reformat TextFormatting

--- src/main/java/me/ksyz/armorhud/utils/TextFormatting.java
+++ src/main/java/me/ksyz/armorhud/utils/TextFormatting.java
@@ -16,7 +16,8 @@ public enum TextFormatting {
   AQUA('b', -11141121),
   RED('c', -43691),
   LIGHT_PURPLE('d', -43521),
-  YELLOW('e', -171), WHITE('f', -1),
+  YELLOW('e', -171),
+  WHITE('f', -1),
   MAGIC('k', 0),
   BOLD('l', 0),
   STRIKETHROUGH('m', 0),
@@ -29,7 +30,7 @@ public enum TextFormatting {
 
   public static final char COLOR_CHAR = '\u00A7';
 
-  TextFormatting(char code, int rgb) {
+  TextFormatting(final char code, final int rgb) {
     this.rgb = rgb;
     this.toString = new String(new char[]{COLOR_CHAR, code});
   }
@@ -43,9 +44,9 @@ public enum TextFormatting {
     return this.rgb;
   }
 
-  public static String translate(String text) {
+  public static String translate(final String text) {
     char[] b = text.toCharArray();
-    for (int i = 0; i < b.length - 1; i++) {
+    for (int i = 0; i < b.length - 1; ++i) {
       if (b[i] == '&' && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i + 1]) > -1) {
         b[i] = TextFormatting.COLOR_CHAR;
         b[i + 1] = Character.toLowerCase(b[i + 1]);
